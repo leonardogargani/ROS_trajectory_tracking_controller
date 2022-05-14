bdfe35d708395e7a300edbf298c5d3d8e76ba678
leonardogargani <leonardo.gargani@mail.polimi.it>
2022-05-14 15:10:06 +0200

    Add local and global costmaps for DWA

--- src/dwa_demo/src/demo.cpp
+++ src/dwa_demo/src/demo.cpp
@@ -14,35 +14,15 @@ int main(int argc, char **argv)
     tf2_ros::Buffer tfBuffer(ros::Duration(10));
     tf2_ros::TransformListener tfListener(tfBuffer);
         
-    costmap_2d::Costmap2DROS my_costmap("my_costmap", tfBuffer);
+    costmap_2d::Costmap2DROS my_local_costmap("my_local_costmap", tfBuffer);
+    costmap_2d::Costmap2DROS my_global_costmap("my_global_costmap", tfBuffer);
 
-    my_costmap.start();
+    my_local_costmap.start();
+    my_global_costmap.start();
 
     dwa_local_planner::DWAPlannerROS dp;
-    dp.initialize("my_dwa_planner", &tfBuffer, &my_costmap);
+    dp.initialize("my_dwa_planner", &tfBuffer, &my_local_costmap);
     
-    
-    /*
-     * Come usare DWA con un nostra path come reference (dal service).
-     * 
-    1) Ottieni path di riferimento chiamando servizio eight_trajgen e trasfomalo in un vettore di PoseStamped.
-    	1.a) Passa il path al planner usando setPlan().
-    		 setPlan() prende come argomento un vettore di PoseStamped.
-    
-    2) Crea variabile di tipo PoseStamped e Twist.
-       PoseStamped va riempito con position, orientation e velocity correnti.
-       Twist sarà riempito con le nuove velocità --> sia lineare che angolare.
-       2.a) (forse) dividi la omega in omega_dx e omega_sx.
-    
-    3) Chiama dwaComputeVelocityCommands() passandogli i parametri appena dichiarati.
-    
-    4) Usa velocità ottenute per calcolare il nuovo stato [x, y, theta] come diffdrive_kin_ode.
-    
-    5) Ripeti da punto 2 fino a fine path.
-       La fine del path forse la si trova con isGoalReached().
-    */
-    
-
     ros::ServiceClient client;
     diffdrive_kin_ctrl::GenerateDesiredPathService srv;
     ros::NodeHandle Handle;
@@ -74,30 +54,70 @@ int main(int argc, char **argv)
 
     // create twist message to be populated by the local planner
     geometry_msgs::Twist dwa_cmd_vel;
-
+    dwa_cmd_vel.angular.x = -1;
+    dwa_cmd_vel.angular.y = -1;
+    dwa_cmd_vel.angular.z = -1;
+    dwa_cmd_vel.linear.x = -1;
+    dwa_cmd_vel.linear.y = -1;
+    dwa_cmd_vel.linear.z = -1;
     geometry_msgs::PoseStamped l_global_pose;
-    my_costmap.getRobotPose(l_global_pose);
 
     while(!dp.isGoalReached()) {
 
+        ROS_INFO("pt.2a");
+        my_global_costmap.getRobotPose(l_global_pose);
+
         // update global costmap
-        my_costmap.updateMap();
+        my_local_costmap.updateMap();
+        my_global_costmap.updateMap();
+
+        ROS_INFO("pt.2b");
 
         // compute velocity commands using DWA
-        if (dp.dwaComputeVelocityCommands(l_global_pose, dwa_cmd_vel)) {
+        //if (dp.dwaComputeVelocityCommands(l_global_pose, dwa_cmd_vel) == true) {
+        if (dp.computeVelocityCommands(dwa_cmd_vel) == true) {
             ROS_INFO("DWA compute cmd_vel: SUCCESS");
         } else {
             ROS_ERROR("DWA compute cmd_vel: FAILED");
         }
+        
+
+        // https://answers.ros.org/question/376753/dwa-local-planner-as-a-stand-alone-c-library/
+        // rosrun rqt_logger_level rqt_logger_level
+
+
+        /*
+            linear: 
+            x: 0
+            y: 0.1
+            z: 0
+            angular: 
+            x: 0
+            y: 0
+            z: 0.126316
+        */
+        //std::cout << dwa_cmd_vel << std::endl;
+        /*
+            pose: 
+            position: 
+                x: 0
+                y: 0
+                z: 0
+            orientation: 
+                x: 0
+                y: 0
+                z: 0
+                w: 1
+        */
+        //std::cout << l_global_pose << std::endl;
+
+        // it seems to work => we just need to send dwa_cmd_vel to the simulator to actually make the robot move
 
     }
 
+    ROS_INFO("pt.3");
 
     // dwaComputeVelocityCommands -> odom_helper_.getRobotVel (line 196)
- 
-
-
-
 
     return (0);
 
